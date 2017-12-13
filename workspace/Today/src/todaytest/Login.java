665dbcba05a9ff4309dc8ffca6301bbced3d2fdc
NarmadhaKuppusamy <connectnarmi93@gmail.com>
2017-12-13 21:33:29 +0530

    Testcase Updated

---
+++ workspace/Today/src/todaytest/Login.java
@@ -0,0 +1,15 @@
+package todaytest;
+
+public class Login {
+	public String getName(String st) {
+		String name = "Narmi";
+		name = name + st;
+		return name;
+	}
+
+public static void main (String[]args)
+{
+
+
+}   
+}
